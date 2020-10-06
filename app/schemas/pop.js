8ecca831046b40321ff1b9e5c6eb5b6a793cfbc3
Flyworkz <pedron.dorian@gmail.com>
2020-10-06 23:05:28 +0200

    Ajout id dans schema pour update

--- app/schemas/pop.js
+++ app/schemas/pop.js
@@ -1,6 +1,7 @@
 const Joi = require('joi');
 
 const popSchema = Joi.object({
+    id: Joi.number().integer().min(1),
     figurine_label: Joi.number().integer().min(1).required(),
     collection: Joi.string().required(),
     label: Joi.string().required(),
